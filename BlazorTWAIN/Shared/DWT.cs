75e1c30d59a9f4ab6cad41f1bc33aeb582d6ef0c
xulihang <xulihanghai@163.com>
2021-06-08 10:42:06 +0800

    Update with JavaScript isolation

---
+++ BlazorTWAIN/Shared/DWT.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTWAIN.Shared
+{
+    public class DWT
+    {
+        private IJSObjectReference module;
+        [Inject]
+        IJSRuntime JS { get; set; }
+        private DWT()
+        {            
+        }
+        public static async Task<DWT> CreateAsync(IJSRuntime JS)
+        {
+            DWT dwt = new DWT();
+            dwt.JS = JS;
+            await dwt.LoadJSAsync();
+            return dwt;
+        }
+
+        private async Task LoadJSAsync()
+        {
+            module = await JS.InvokeAsync<IJSObjectReference>("import", "./js/DWT.js");
+        }
+
+        public async Task CreateDWT()
+        {
+            await module.InvokeVoidAsync("CreateDWT");
+        }
+
+        public async Task Scan()
+        {
+            await module.InvokeVoidAsync("Scan");
+        }
+
+        public async Task LoadImage()
+        {
+            await module.InvokeVoidAsync("LoadImage");
+        }
+
+        public async Task Save()
+        {
+            await module.InvokeVoidAsync("Save");
+        }
+
+        public async Task<Boolean> IsDesktop()
+        {
+            return await module.InvokeAsync<Boolean>("isDesktop");
+        }
+
+        protected virtual async ValueTask DisposeAsync()
+        {
+            await module.DisposeAsync();
+        }
+    }
+}
--- BlazorTWAIN_Server/Startup.cs
+++ BlazorTWAIN_Server/Startup.cs
@@ -1,3 +1,4 @@
+using BlazorTWAIN_Server.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -27,6 +28,7 @@ namespace BlazorTWAIN_Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
